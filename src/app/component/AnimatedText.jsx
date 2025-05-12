c7a292e80753ba84042ce7b94996599bff3c43c5
Louisaflor <louisa.yonzon6@gmail.com>
2025-05-12 12:32:15 -0700

    refactoring to create default animations
    removing the attributes in the motion.span and putting it in a variable to customize our defaults.

--- src/app/component/AnimatedText.jsx
+++ src/app/component/AnimatedText.jsx
@@ -1,12 +1,24 @@
+"use client"; 
 import React from 'react';
-import { motion } from "motion/react";
+import { motion } from 'framer-motion';
 
-export default function AnimatedText({el: Wrapper = 'p' , text, className}) {
+export default function AnimatedText({ el: Wrapper = 'p', text, className }) {
+  const defaultAnimation= {
+     hidden: {
+      opacity: 0
+     },
+     visible: {
+      opacity: 1
+     }
+  }
+
   return (
     <Wrapper className={className}>
-        <motion.span>{text.split('').map(char => {
-            <span>{char}</span>
-        })}</motion.span>
+      <motion.span>
+        {text.split('').map((char, i) => (
+          <motion.span initial={{opacity: 0 }} animate={{opacity: 1 }} key={i}>{char}</motion.span>
+        ))}
+      </motion.span>
     </Wrapper>
-  )
+  );
 }
